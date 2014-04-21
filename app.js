10b7bd7939103cf79944f8794425ec3cc9036711
Matt Lea <mlea@schematical.com>
2014-04-21 07:23:39 -0500

    moving along

--- app.js
+++ app.js
@@ -15,47 +15,44 @@ app.locals.partials._meta = '_meta';
 app.locals.partials._meta_footer = '_meta_footer';
 app.locals.partials._modal = '_modal';
 app.locals.partials._section = '_section';
+app.locals.partials._product = '_product';
+
 
 app.model.Section.find(function(err, sections){
+    if(err) throw err;
     app.locals.sections = [];
-    for(var i in sections){
-        app.locals.sections[i] = sections[i].toObject();
-    }
+    async.eachSeries(
+        sections,
+        function(section, cb){
+            app.model.Product.find(
+                { section:section._id },
+                function(err, products){
+                    if(err) throw err;
+                    var section_obj = section.toObject();
+                    section_obj.products = [];
+                    for(var i in products){
+                        var product_objs = products[i].toObject();
+                        product_objs._index = i;
+                        section_obj.products.push(product_objs);
+                    }
+                    app.locals.sections.push(section_obj);
+                    return cb();
+                }
+            );
+        },
+        function(){
+            //Were Done
+            console.log(app.locals.sections);
+        }
+    )
 });
 
-
-app.use(function(req, res, next){
-    async.series([
-        function(cb){
-
-            return next();
-/*
-            //Shitty hack
-            if(req.user){
-                return next();
-            }
-            app.model.Account.findOne({ email:'mlea@schematical.com' }, function(err, account){
-
-                req.login(account, function(err) {
-                    if (err) { return next(err); }
-                    return cb();
-                });
-
-            })*/
-
-        }
-    ],
-    next
-    );
-
-
-})
-//app._refresh_locals = require('./lib/modules/timeout')(app);
-//setTimeout(app._refresh_locals, 5* 60 * 1000);
-//app._refresh_locals();
 
 
 var routes = require(__dirname + '/lib/routes');
 routes(app);
+
+var amazon_routes = require(__dirname + '/lib/routes/amazon');
+amazon_routes(app);
 
 app.start();
